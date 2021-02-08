af967cb321d54ae6bdc95c324c44183e58c805e1
Victorien Brun <b.victorien@hotmail.fr>
2021-02-08 16:40:23 +0100

    Installation de React et gestion des customers et invoices

---
+++ assets/app.js
@@ -0,0 +1,42 @@
+//Les imports importants
+import React from 'react';
+import ReactDOM from "react-dom";
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+
+// start the Stimulus application
+import './bootstrap';
+import NavBar from './js/components/Navbar';
+import HomePage from './js/pages/HomePage';
+import { HashRouter, Switch, Route } from "react-router-dom";
+import CustomersPage from './js/pages/CustomersPage';
+import InvoicesPage from './js/pages/InvoicesPage';
+
+//Javascript
+console.log("Hello World!!");
+
+const App = () => {
+  return (
+  <HashRouter>
+    <NavBar />
+
+    <main className="container pt-5">
+        <Switch>
+          <Route path="/invoices" component={InvoicesPage}/>
+          <Route path="/customers" component={CustomersPage}/>
+          <Route path="/" component={HomePage}/>
+        </Switch>
+    </main>
+  </HashRouter>
+  );
+};
+
+const rootElement = document.querySelector('#app');
+ReactDOM.render(<App />, rootElement);
